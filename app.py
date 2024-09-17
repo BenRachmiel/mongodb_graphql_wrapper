2672808d090b045ad29cc82c05152ad012802006
benjaminbenrachmiel <bvf2104@columbia.edu>
2024-09-17 18:50:32 +0300

    Working API, added some example queries and mutations, generated requirements.txt

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from ariadne import QueryType, MutationType, make_executable_schema, graphql_sync, load_schema_from_path
 from ariadne.explorer import ExplorerPlayground
-from bson.objectid import ObjectId
 
 # Import custom scalars
 from scalars import (
@@ -33,7 +32,22 @@ db = mongo.db
 
 # Make the executable schema using the loaded schema file
 loaded_schema_path = load_schema_from_path("schema.graphql")
-schema = make_executable_schema(loaded_schema_path, query, mutation)
+query = QueryType()
+mutation = MutationType()
+query.set_field("retrieveRecentHistory", resolve_retrieve_recent_history)
+query.set_field("retrieveQuery", resolve_retrieve_query)
+mutation.set_field("saveQuery", resolve_save_query)
+loaded_schema = load_schema_from_path("schema.graphql")
+schema = make_executable_schema(
+    loaded_schema,
+    [query, mutation],
+    latitude_scalar,
+    longitude_scalar,
+    angle_scalar,
+    nonnegative_float_scalar,
+    priority_scalar,
+    quality_scalar
+)
 
 
 # GraphQL Playground route for testing the API
@@ -46,7 +60,8 @@ def graphql_playground():
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
     data = request.get_json()
-    success, result = graphql_sync(schema, data, context_value=request, debug=app.debug)
+    context = {"db": db, "request": request}
+    success, result = graphql_sync(schema, data, context_value=context, debug=app.debug)
     return jsonify(result)
 
 
--- resolvers.py
+++ resolvers.py
@@ -1,16 +1,21 @@
-# resolvers.py
+def resolve_retrieve_recent_history(_, info, limit=None):
+    limit = limit or 10
+    db = info.context['db']
+    recent_history = list(db.queries.find().sort("_id", -1).limit(limit))
+    return recent_history
 
-def resolve_retrieve_recent_history(_, info, input=None):
-    # Implement your logic here
-    # For demonstration, returning an empty list
-    return []
 
-def resolve_retrieve_query(_, info, input):
-    # Implement your logic here
-    # For demonstration, returning None
-    return None
+def resolve_retrieve_query(_, info, name):
+    query = {"name": {"$regex": name, "$options": "i"}}
+
+    result = info.context['db'].queries.find_one(query)
+
+    return result
+
 
 def resolve_save_query(_, info, input):
-    # Implement your logic here
-    # For demonstration, returning a success message
-    return "Query saved successfully!"
+    # Insert the new data into MongoDB
+    insert_result = info.context['db'].queries.insert_one(input)
+
+    # Return the inserted object's ID
+    return str(insert_result.inserted_id)
